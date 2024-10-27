1ebec2613920422351d69805e3f50f2bdb32e3a5
genry <renegadeworking@gmail.com>
2024-10-27 23:25:54 +0300

    longest

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,27 @@
 package io.genry.wooko
 
 fun main() {
-
-    makeComplement("ATTGC")
+//    makeComplement("ATTGC")
+    longest(s1 = "xyaabbbccccdefww", s2 = "xxxxyyyyabklmopq")
 }
+
+/**
+ * Возьмите 2 строки s1 и s2, содержащие только буквы от a до z.
+ * Верните новую отсортированную строку (в алфавитном порядке),
+ * максимально длинную, содержащую разные буквы — каждая буква берется только один раз — из s1 или s2.
+ *
+ * a = "xyaabbbccccdefww"
+ * b = "xxxxyyyyabklmopq"
+ * longest(a, b) -> "abcdefklmopqwxy"
+ *
+ * a = "abcdefghijklmnopqrstuvwxyz"
+ * longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+ * */
+
+fun longest(s1: String, s2: String): String {
+    return (s1 + s2).toSortedSet().joinToString("")
+}
+
 
 /***
  * Дезоксирибонуклеиновая кислота (ДНК) — это химическое вещество, найденное в ядре клеток, которое несет "инструкции" для развития и функционирования живых организмов.

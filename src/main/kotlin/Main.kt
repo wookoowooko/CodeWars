179aaa1bd0ce9a57011243aa1c274899878bcae8
genry <renegadeworking@gmail.com>
2024-10-28 12:28:02 +0300

    find

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
+import functions.find
+
 fun main() {
 //   makeComplement("ATTGC")
 //   longest(s1 = "xyaabbbccccdefww", s2 = "xxxxyyyyabklmopq")
 //   accum("ZpglnRxqenU")
 //    disemvowel("This website is for losers LOL!")
+    find(arrayOf(160, 3, 1719, 19, 11, 13, -21))
 }
 
 
